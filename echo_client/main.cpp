2cbca614f2410ee34a408b38f423c6b949332e06
Polina <44026273+M-Polina@users.noreply.github.com>
2023-12-20 14:55:21 +0300

    add boost beast echo client

--- echo_client/main.cpp
+++ echo_client/main.cpp
@@ -1,78 +1,186 @@
-#include <cstring>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/strand.hpp>
+#include <cstdlib>
 #include <iostream>
-#include <boost/asio.hpp>
+#include <memory>
+#include <string>
 
-using boost::asio::ip::tcp;
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace websocket = beast::websocket;
+namespace net = boost::asio;
+using tcp = boost::asio::ip::tcp;
 
-class Echo_client {
+void fail(beast::error_code ec, char const* what)
+{
+    std::cerr << what << ": " << ec.message() << "\n";
+}
+
+class session : public std::enable_shared_from_this<session>
+{
+    tcp::resolver resolver_;
+    websocket::stream<beast::tcp_stream> ws_;
+    beast::flat_buffer buffer_;
+    std::string host_;
+    std::string text_;
+
 public:
-    Echo_client(boost::asio::io_service &io_service, std::string host, std::string port)
-            : io_service_(io_service), resolver_(io_service), query_(boost::asio::ip::address::from_string(host), std::atoi(port.data())),
-              socket_(io_service){
-        socket_connect();
-        start();
+    explicit
+    session(net::io_context& ioc)
+        : resolver_(net::make_strand(ioc))
+        , ws_(net::make_strand(ioc))
+    {
     }
 
-    ~Echo_client() {
-        socket_.close();
+    void
+    run(
+        char const* host,
+        char const* port)
+    {
+        host_ = host;
+        text_ = "";
+
+        resolver_.async_resolve(
+            host,
+            port,
+            beast::bind_front_handler(
+                &session::on_resolve,
+                shared_from_this()));
     }
 
-    void socket_connect(){
-        socket_.connect(query_);
+    void
+    on_resolve(
+        beast::error_code ec,
+        tcp::resolver::results_type results)
+    {
+        if(ec)
+            return fail(ec, "resolve");
+
+        beast::get_lowest_layer(ws_).expires_after(std::chrono::seconds(30));
+
+        beast::get_lowest_layer(ws_).async_connect(
+            results,
+            beast::bind_front_handler(
+                &session::on_connect,
+                shared_from_this()));
     }
 
-    void start(){
-        get_request();
+    void
+    on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type)
+    {
+        if(ec)
+            return fail(ec, "connect");
+
+        beast::get_lowest_layer(ws_).expires_never();
+
+        ws_.set_option(
+            websocket::stream_base::timeout::suggested(
+                beast::role_type::client));
+
+        ws_.set_option(websocket::stream_base::decorator(
+            [](websocket::request_type& req)
+            {
+                req.set(http::field::user_agent,
+                    std::string(BOOST_BEAST_VERSION_STRING) +
+                        " websocket-client-async");
+            }));
+
+        ws_.async_handshake(host_, "/",
+            beast::bind_front_handler(
+                &session::on_handshake,
+                shared_from_this()));
     }
 
-    void get_request(){
+    std::string get_request_text() {
         std::cout << "Enter message: ";
         std::string request;
-
         std::getline(std::cin, request);
-        request.append("\r\n\r\n");
-        std::cout << request.size() << std::endl;
-
-        handle_write(request);
+        return request;
     }
 
-    void handle_write(std::string request){
-        boost::asio::write(socket_, boost::asio::buffer(request, request.size()));
-        handle_get_reply( request.size());
+    void print_reply_text() {
+        std::cout << "Reply: " << beast::make_printable(buffer_.data()) << std::endl;
     }
 
-    void handle_get_reply(size_t request_length)
+    void
+    on_handshake(beast::error_code ec)
     {
-        boost::asio::streambuf reply;
-        boost::asio::read_until(socket_, reply, "\r\n\r\n");
-        std::istream reply_istream(&reply);
-        std::string reply_str;
-        std::getline(reply_istream, reply_str);
-        std::cout << reply_str.size() << std::endl;
-        reply_str.erase(reply_str.size() - 1);
-        std::cout << "Reply: " << reply_str << std::endl;
-        get_request();
+        if(ec)
+            return fail(ec, "handshake");
+
+        std::string text = get_request_text();
+        text_ = text;
+        ws_.async_write(
+            net::buffer(text),
+            beast::bind_front_handler(
+                &session::on_write,
+                shared_from_this()));
     }
 
-private:
-    boost::asio::io_service &io_service_;
-    tcp::resolver resolver_;
-    tcp::endpoint query_;
-    tcp::resolver::iterator iterator_;
-    tcp::socket socket_;
+    void
+    on_write(
+        beast::error_code ec,
+        std::size_t bytes_transferred)
+    {
+        boost::ignore_unused(bytes_transferred);
+
+        if(ec)
+            return fail(ec, "write");
+
+        ws_.async_read(
+            buffer_,
+            beast::bind_front_handler(
+                &session::on_read,
+                shared_from_this()));
+    }
+
+    void
+    on_read(
+        beast::error_code ec,
+        std::size_t bytes_transferred)
+    {
+        print_reply_text();
+        boost::ignore_unused(bytes_transferred);
+
+        buffer_.clear();
+        on_handshake(ec);
+
+//        if(ec)
+//            return fail(ec, "read");
+//
+//        ws_.async_close(websocket::close_code::normal,
+//            beast::bind_front_handler(
+//                &session::on_close,
+//                shared_from_this()));
+    }
+
+    void
+    on_close(beast::error_code ec)
+    {
+        if(ec)
+            return fail(ec, "close");
+
+        std::cout << "Session closed.";
+    }
 };
 
-
-int main(int argc, char *argv[]) {
+int main(int argc, char** argv)
+{
     try {
-        boost::asio::io_service io_service;
+        auto const host = "127.0.0.1";
+        auto const port = "8080";
 
-        Echo_client client(io_service, "127.0.0.1", "80");
+        net::io_context ioc;
+
+        std::make_shared<session>(ioc)->run(host, port);
+
+        ioc.run();
 
     }
     catch (std::exception &e) {
         std::cerr << "Exception: " << e.what() << "\n";
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
